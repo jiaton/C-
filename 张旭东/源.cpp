39aea12e75930a2ae06b3116b5c6a5725284d825
dalao <792911138@qq.com>
2017-12-06 20:44:32 +0800

    张旭东

--- 张旭东/源.cpp
+++ 张旭东/源.cpp
@@ -1,26 +1,28 @@
-#include<iostream>
-using namespace std;
-int main() {
-	int C;
-	cin >> C;
-	for (int i = 0; i < C; i++) {
-		int N;
-		cin >> N;
-		int Si[60];
-		for (int i = 0; i < N; i++) {
-			cin >> Si[i];
-		}
-		float ave=0;
-		for (int i = 0; i < N; i++) {
-			ave += Si[i];
-		}
-		ave /= N;
-		int count=0;
-		for (int i = 0; i < N; i++) {
-			if (Si[i] >= ave) count++;
-		}
-		printf("%.2f", ave);
-		cout << " ";
-		cout << count << endl;
-	}
+/*11.24 钟卓杭*/ 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h> 
+const int N = 6;
+
+int cmp(const void *a,const void *b)//带入无类型指针常量a,b参数， 
+{ return strcmp(*(char **)a,*(char**)b);//strcmp传入字符串首地址 
+}//定义一个比较函数 
+
+void sort(char * language[])
+{
+  qsort(language,N,sizeof(char *),cmp);
+} 
+
+void output(char * language[])
+{ int i=0;
+  for(i;i<N;i++)
+  {
+  	printf("%s\n",language[i]);
+  }
 }
+int main()
+{ char *Language[] = {"PASCAL","BASIC","C/C++","Fortran","JAVA","Turbo C"};
+  sort (Language);
+  output (Language); 
+  return 0;
+} 
